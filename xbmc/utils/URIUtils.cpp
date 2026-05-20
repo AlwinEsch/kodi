xbmc/xbmc

[utils] add allow "URIUtils::HasExtension(...)" with vector list
This to clean and improve the work, as no more the string edit to split needs done.

xbmc/utils/URIUtils.cpp
@@ -114,6 +114,34 @@ bool URIUtils::HasExtension(const std::string& strFileName, const std::string& s
   return false;
 }
 
+bool URIUtils::HasExtension(const CURL& url, const std::vector<std::string>& extList)
+{
+  return HasExtension(url.GetFileName(), extList);
+}
+
+bool URIUtils::HasExtension(const std::string& strFileName, const std::vector<std::string>& extList)
+{
+  if (IsURL(strFileName))
+  {
+    const CURL url(strFileName);
+    return HasExtension(url.GetFileName(), extList);
+  }
+
+  const size_t pos = strFileName.find_last_of("./\\");
+  if (pos == std::string::npos || strFileName[pos] != '.')
+    return false;
+
+  const std::string extension = strFileName.substr(pos);
+
+  for (const auto& ext : extList)
+  {
+    if (StringUtils::EndsWithNoCase(ext, extension))
+      return true;
+  }
+
+  return false;
+}
+
 void URIUtils::RemoveExtension(std::string& strFileName)
 {
   if(IsURL(strFileName))

xbmc/utils/URIUtils.h
@@ -70,6 +70,39 @@ public:
   static bool HasExtension(const std::string& strFileName, const std::string& strExtensions);
   static bool HasExtension(const CURL& url, const std::string& strExtensions);
 
+  /*!
+   * \brief Check if filename have any of the listed extensions
+   *
+   * \param[in] strFileName Path or URL to check
+   * \param[in] extList List of '.' prefixed lowercase extensions
+   * \return \e true if strFileName have any one of the extensions.
+   *
+   * \note The check is case insensitive for strFileName, but requires
+   * extList content to be lowercase. Returns false when strFileName or
+   * extList is empty.
+   *
+   * \sa GetExtension
+   *
+   * ------------------------------------------------------------------------
+   *
+   * **Example:**
+   * ~~~~~~~~~~~~~{.cpp}
+   * static const std::vector<std::string> list = {
+   *   ".mkv",
+   *   ".mp4",
+   *   ".avi",
+   *   ".m4v"
+   * };
+   *
+   * if (URIUtils::HasExtension(fileNameAndPath, list))
+   * {
+   *   ...
+   * }
+   * ~~~~~~~~~~~~~
+   */
+  static bool HasExtension(const std::string& strFileName, const std::vector<std::string>& extList);
+  static bool HasExtension(const CURL& url, const std::vector<std::string>& extList);
+
   static void RemoveExtension(std::string& strFileName);
   static std::string ReplaceExtension(const std::string& strFile,
                                      const std::string& strNewExtension);

AlwinEsch/kodi

xbmc/utils/URIUtils.cpp
@@ -278,6 +278,34 @@ bool URIUtils::HasExtension(const std::string& path, std::string_view extensions
   return FindExtension(path, extensions, FindExtensions::ONLY_IN_LIST) != NO_EXTENSION;
 }
 
+bool URIUtils::HasExtension(const CURL& url, const std::vector<std::string>& extList)
+{
+  return HasExtension(url.GetFileName(), extList);
+}
+
+bool URIUtils::HasExtension(const std::string& path, const std::vector<std::string>& extList)
+{
+  if (IsURL(path))
+  {
+    const CURL url(path);
+    return HasExtension(url.GetFileName(), extList);
+  }
+
+  const size_t pos = path.find_last_of("./\\");
+  if (pos == std::string::npos || path[pos] != '.')
+    return false;
+
+  const std::string extension = path.substr(pos);
+
+  for (const auto& ext : extList)
+  {
+    if (StringUtils::EndsWithNoCase(ext, extension))
+      return true;
+  }
+
+  return false;
+}
+
 void URIUtils::RemoveExtension(std::string& path)
 {
   if (IsURL(path))

xbmc/utils/URIUtils.h
@@ -77,6 +77,40 @@ public:
   static bool HasExtension(const std::string& path, std::string_view extensions);
   static bool HasExtension(const CURL& url, std::string_view strExtensions);
 
+
+  /*!
+   * \brief Check if filename have any of the listed extensions
+   *
+   * \param[in] path Path or URL to check
+   * \param[in] extList List of '.' prefixed lowercase extensions
+   * \return \e true if path have any one of the extensions.
+   *
+   * \note The check is case insensitive for path, but requires
+   * extList content to be lowercase. Returns false when path or
+   * extList is empty.
+   *
+   * \sa GetExtension
+   *
+   * ------------------------------------------------------------------------
+   *
+   * **Example:**
+   * ~~~~~~~~~~~~~{.cpp}
+   * static const std::vector<std::string> list = {
+   *   ".mkv",
+   *   ".mp4",
+   *   ".avi",
+   *   ".m4v"
+   * };
+   *
+   * if (URIUtils::HasExtension(fileNameAndPath, list))
+   * {
+   *   ...
+   * }
+   * ~~~~~~~~~~~~~
+   */
+  static bool HasExtension(const std::string& path, const std::vector<std::string>& extList);
+  static bool HasExtension(const CURL& url, const std::vector<std::string>& extList);
+
   static void RemoveExtension(std::string& path);
   static std::string ReplaceExtension(const std::string& path, const std::string& newExtension);
   static void Split(const std::string& strFileNameAndPath,